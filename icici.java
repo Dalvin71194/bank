6765b80bae519246895cd60b4b82cfb14c0c61a3
Dalvin <118263960+Dalvin71194@users.noreply.github.com>
2022-12-02 15:48:34 +0530

    Add files via upload

---
+++ icici.java
@@ -0,0 +1,20 @@
+y67u/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bank;
+
+/**
+ *
+ * @author PGLAB
+ */
+public class icici {
+     String bank_name;
+    Float interest;
+    String display_interest()
+    {
+        System.out.println("the interest is"+"7%");
+   return(interest+bank_name); }
+    
+}
